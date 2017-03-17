beaf4d982aed4983b569cdb5fcca4602b20988fb
Kyle Lee <hello.kylelee@gmail.com>
2017-03-17 16:16:43 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,3 @@ xhr.onerror = function() {
 xhr.send();
 
 //with fetch
-
-const token = '227b8b59769946ae3c12de6d379ad6dae1a1f53a';
-
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-})
-  .then(res => res.json())
-  .then(json => console.log(json));
